b8d8dbe8377083fcc2f08246c389032fc10b7512
Yuvraj Singh Gohil <yuvrajgohil1817@gmail.com>
2025-04-12 19:12:52 +0530

    Real-time chat functionality implemented successfully using SocketIo

---
+++ socket/app.js
@@ -0,0 +1,49 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+    cors: {
+        origin: "http://localhost:5173"
+    },
+});
+
+// io.on("connection", (socket) => {
+//     socket.on("test", (data) => {
+//         console.log(data)
+//     })
+// })
+
+let onlineUser = [];
+
+const addUser = (userId, socketId) => {
+    const userExits = onlineUser.find((user) => user.userId === userId);
+    if (!userExits) {
+        onlineUser.push({ userId, socketId });
+    }
+};
+
+const removeUser = (socketId) => {
+    onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+    return onlineUser.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+    socket.on("newUser", (userId) => {
+        addUser(userId, socket.id);
+        console.log(onlineUser)
+    });
+
+    socket.on("sendMessage", ({ receiverId, data }) => {
+        console.log("ID-->", receiverId, data)
+        const receiver = getUser(receiverId);
+        io.to(receiver.socketId).emit("getMessage", data);
+    });
+
+    socket.on("disconnect", () => {
+        removeUser(socket.id);
+    });
+});
+
+io.listen("4000");
